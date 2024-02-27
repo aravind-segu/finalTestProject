21c088ed42ef61e8c626864524a00131951c6017
aravind-segu <aravind.segu@databricks.com>
2024-02-27 11:09:32 -0800

    Changes

--- finalTestProject/deployment/batch_inference/notebooks/BatchInference.py
+++ finalTestProject/deployment/batch_inference/notebooks/BatchInference.py
@@ -28,7 +28,7 @@ dbutils.widgets.text("input_table_name", "", label="Input Table Name")
 dbutils.widgets.text("output_table_name", "", label="Output Table Name")
 # Unity Catalog registered model name to use for the trained mode.
 dbutils.widgets.text(
-    "model_name", "dev.finalTestProject.finalTestProject-model", label="Full (Three-Level) Model Name"
+    "model_name", "dev.aravind_test.finalTestProject-model", label="Full (Three-Level) Model Name"
 )
 
 # COMMAND ----------
